92e5877cc86cb98016ea9f3712368bcfbe77176d
ISABELLY LIMA DA SILVA <24122387@CFP127ALUNO.LOCAL>
2025-03-11 12:08:46 -0300

    primeira página iniciada

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const imagem = [
+    {nome: 'bandeira', icon: 'image.png'},
+    {nome: 'insta', icon:'insta.png'},
+    {nome: 'logo', icon: 'logo.png'},
+    {nome: 'calendario', icon: 'calendario.png'},
+    {nome: 'ajuda', icon: 'ajuda.png'},
+    {nome: 'face', icon: 'face.png'},
+    {nome: 'menssager', icon: 'menssager.png'},
+    {nome: 'perfil', icon: 'perfil.png'},
+    {nome: 'pesquisa', icon: 'pesquisa.png'}
+]
