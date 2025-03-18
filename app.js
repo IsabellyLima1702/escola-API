467220e9649ee08dc5ef34928388c9420d9c965a
ISABELLY LIMA DA SILVA <24122387@CFP127ALUNO.LOCAL>
2025-03-18 08:46:09 -0300

    layout da página ok

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 'use strict'
 
-const imagem = [
-    {nome: 'bandeira', icon: 'image.png'},
-    {nome: 'insta', icon:'insta.png'},
-    {nome: 'logo', icon: 'logo.png'},
-    {nome: 'calendario', icon: 'calendario.png'},
-    {nome: 'ajuda', icon: 'ajuda.png'},
-    {nome: 'face', icon: 'face.png'},
-    {nome: 'menssager', icon: 'menssager.png'},
-    {nome: 'perfil', icon: 'perfil.png'},
-    {nome: 'pesquisa', icon: 'pesquisa.png'}
-]
+async function pesquisarFotos (){
+    const url = `https://api.data.gov/ed/collegescorecard/v1/schools?api_key=wCylfStKoX7qrU7gbP3iaXLsa0DMBM9p0p7rU13c&${school.city}fields=latest.school.faculty_salary,school.name,latest.student.size,school.state,school.city,latest.school.school_url,%20latest.school.faculty_salary,latest.school.price_calculator_url`
+
+    const response = await fetch(url)
+    const data = await response.json()
+ 
+    return data.message
+}
+
+function criarImagem(link){
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img')
+    novaImg.src   = link
+
+    galeria.appendChild(novaImg)
+}
+
+async function preencherFotos (){
+    const escola = document.getElementById('escola').value
+    const fotos = await pesquisarFotos(escola)
+    const galeria = document.getElementById('galeria')
+
+    galeria.replaceChildren('')
+    fotos.forEach(criarImagem)
+    console.log(fotos)
+}
+
+document.getElementById('pesquisar').addEventListener('click', preencherFotos)
