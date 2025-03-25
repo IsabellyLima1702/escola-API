4f000f6c827650fe595fb2ab30b50e752221597e
ISABELLY LIMA DA SILVA <24122387@CFP127ALUNO.LOCAL>
2025-03-25 09:54:13 -0300

    tela3 criada

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 async function pesquisarCard(city) {
-    const url = `https://api.data.gov/ed/collegescorecard/v1/schools?api_key=wCylfStKoX7qrU7gbP3iaXLsa0DMBM9p0p7rU13c&school.city=${city}&fields=school.name,school.state,school.city`;
+    const url = `https://api.data.gov/ed/collegescorecard/v1/schools?api_key=wCylfStKoX7qrU7gbP3iaXLsa0DMBM9p0p7rU13c&school.city=${city}&fields=latest.school.faculty_salary,school.name,latest.student.size,school.state,school.city,latest.school.school_url,latest.school.faculty_salary,latest.school.price_calculator_url`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -11,20 +11,27 @@ async function pesquisarCard(city) {
 
 function criarCard(link) {
     const galeria = document.getElementById('galeria');
-    const novoCard = document.createElement('div'); // Usando div para melhor organização
+    const novoCard = document.createElement('div');
     novoCard.classList.add('card');
 
     const nome = link['school.name'];
     const estado = link['school.state'];
     const cidade = link['school.city'];
+    const estudantes = link['latest.student.size'];
+    const site = link['latest.school.school_url'];
+    const salario = link['latest.school.faculty_salary'];
+    const calculadora = link['latest.school.price_calculator_url']
 
     novoCard.innerHTML = `
         <h1>${nome}</h1>
         <h2>Cidade: ${cidade}</h2>
-        <h3>Estado: ${estado}</h3>
+        <h2>Estado: ${estado}</h2>
+        <button class="saiba-mais" data-nome="${nome}" data-estado="${estado}" data-cidade="${cidade}">Saiba Mais</button>
     `;
 
     galeria.appendChild(novoCard);
+    novoCard.querySelector('.saiba-mais').addEventListener('click', exibirDetalhes);
+
 }
 
 async function preencherPagina() {
@@ -40,3 +47,37 @@ async function preencherPagina() {
 
 document.getElementById('pesquisar')
     .addEventListener('click', preencherPagina);
+
+function exibirDetalhes(novo) {
+    const nome = novo.target.dataset.nome;
+    const estado = novo.target.dataset.estado;
+    const cidade = novo.target.dataset.cidade;
+    const estudantes = novo.target.dataset.estudantes;
+    const site = novo.target.dataset.site;
+    const salario = novo.target.dataset.salario;
+    const calculadora = novo.target.dataset.calculadora;
+    
+        
+    const detalhesCard = document.createElement('div');
+    detalhesCard.classList.add('detalhes-card');
+        
+    detalhesCard.innerHTML = `
+        <h1>${nome}</h1>
+        <h3>Cidade: ${cidade}</h3>
+        <h3>Estado: ${estado}</h3>
+        <h3>Estudantes recentes: ${estudantes}</h3>
+        <h3>Página oficial: ${site}</h3>
+        <h3>Salário da faculdade: ${salario}</h3>
+        <h3>Calculadora financeira escolar: ${calculadora} </h3>
+        <button class="voltar">Voltar</button>
+    `;
+    const galeria = document.getElementById('galeria');
+    galeria.replaceChildren(detalhesCard);
+        
+    detalhesCard.querySelector('.voltar').addEventListener('click', preencherPagina);
+
+    
+}
+
+
+
